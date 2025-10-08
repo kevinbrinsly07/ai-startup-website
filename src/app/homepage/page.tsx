207f014f519bdc2d5db516eba1790b8a387a0276
kevinbrinsly <brinslykevin@gmail.com>
2025-10-08 12:58:46 +0530

    Initial Commit

---
+++ src/app/homepage/page.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Nav from "@/components/Nav";
+
+const page = () => {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/components/Nav.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import Image from "next/image";
+import logo from "../../public/Nav/Logo.svg";
+import downArrow from "../../public/Nav/downArrow.svg";
+
+export default function Nav() {
+  return (
+    <div className="flex flex-col justify-center items-center py-5">
+      <div className="max-w-[1000px] w-full flex flex-row justify-between">
+        <Image src={logo} height={38} width={38} alt="Logo" />
+        <div className="flex flex-row gap-10 text-medium text-[#878687] px-[30px] py-[4px] border-[1px] border-[#878687] rounded-[60px]">
+          <div className="flex flex-row gap-3 justify-center items-center cursor-pointer">
+            <h1>Features</h1>
+            <Image
+              src={downArrow}
+              height={9.5}
+              width={5.2}
+              alt={"down arrow"}
+            />
+          </div>
+          <div className="flex flex-row gap-3 justify-center items-center cursor-pointer">
+            <h1>Developers</h1>
+            <Image
+              src={downArrow}
+              height={9.5}
+              width={5.2}
+              alt={"down arrow"}
+            />
+          </div>
+          <div className="flex flex-row gap-3 justify-center items-center cursor-pointer">
+            <h1>Company</h1>
+            <Image
+              src={downArrow}
+              height={9.5}
+              width={5.2}
+              alt={"down arrow"}
+            />
+          </div>
+          <div className="flex flex-row gap-3 justify-center items-center cursor-pointer">
+            <h1>Blog</h1>
+            <Image
+              src={downArrow}
+              height={9.5}
+              width={5.2}
+              alt={"down arrow"}
+            />
+          </div>
+          <div className="flex flex-row gap-3 justify-center items-center cursor-pointer">
+            <h1>Changelog</h1>
+            <Image
+              src={downArrow}
+              height={9.5}
+              width={5.2}
+              alt={"down arrow"}
+            />
+          </div>
+        </div>
+        <div className="w-[121px] flex justify-center items-center h-[42px] rounded-[12px] border-[1px] border-[#FFFFFF]/[15%]">
+          <div className="w-[109px] h-[30px] rounded-[8px] border-[1px] border-[#FFFFFF]/[15%] bg-[#8C45FF]/[40%] inset-shadow-[#8C45FF] inset-shadow-xs shadow-[inset_0_0_0_2px_rgba(255,255,255,0.15)]"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
